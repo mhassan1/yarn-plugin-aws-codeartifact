2af25b3280ef5bae9545a5a3341ac592d33ac923
mhassan1 <marc.j.hassan@gmail.com>
2022-02-10 18:47:53 -0500

    Use `jest --runInBand` in CI only

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  ...(process.env.CI && { maxWorkers: 1 }),
+  testRegex: '(/__tests__/.*|(\\.|/)test)\\.ts$',
+  moduleFileExtensions: ['ts', 'js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75
+    }
+  },
+  coverageReporters: ['text']
+}
