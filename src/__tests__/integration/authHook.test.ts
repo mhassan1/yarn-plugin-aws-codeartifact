21c30c087cf4ddeae8b1ab8d9ccd07e203b6efdd
mhassan1 <marc.j.hassan@gmail.com>
2022-02-10 09:29:21 -0500

    Detect `yarn dlx` commands more robustly

--- src/__tests__/integration/authHook.test.ts
+++ src/__tests__/integration/authHook.test.ts
@@ -33,6 +33,17 @@ describe('commands that require a registry', () => {
     expect(stdout).toMatch(expectedRegex('aws-profile-my-scope-1'))
   })
 
+  it('should retrieve authorization tokens for AWS CodeArtifact registries when using "workspace my-workspace dlx"', () => {
+    const cwd = join(__dirname, 'fixtures', 'test-package')
+    // this command requires a `FETCH_REGISTRY` for scope `my-scope`, which is an AWS CodeArtifact registry
+    // this command will fail because this is not a real AWS CodeArtifact repository, but it's okay to ignore for this test
+    const stdout = execSync('yarn workspace my-workspace dlx @my-scope/my-package || exit 0', {
+      cwd,
+      env
+    }).toString()
+    expect(stdout).toMatch(expectedRegex('aws-profile-my-scope-1'))
+  })
+
   it('should not retrieve authorization tokens for non-AWS CodeArtifact registries', () => {
     const cwd = join(__dirname, 'fixtures', 'test-package')
     // `yarn` requires a `FETCH_REGISTRY`, which is not an AWS CodeArtifact registry
