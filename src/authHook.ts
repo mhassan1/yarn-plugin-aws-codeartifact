3e1a0e0a7d211687665353dd81f228ac48325943
Michael "Mike" Ferris <micfer@getmaintainx.com>
2026-01-23 17:48:28 -0500

    Ensure aws sso login prints the login code in console (#12)

--- src/authHook.ts
+++ src/authHook.ts
@@ -18,6 +18,7 @@ import { Codeartifact } from '@aws-sdk/client-codeartifact'
 import { defaultProvider } from '@aws-sdk/credential-provider-node'
 import { fromEnv } from '@aws-sdk/credential-provider-env'
 import { chain } from '@aws-sdk/property-provider'
+import { Transform } from 'node:stream';
 
 type TokenGenerator = (
   authorizationTokenParams: AuthorizationTokenParams,
@@ -236,7 +237,18 @@ async function tryLogin<T>(fn: () => Promise<T>) {
     error = err
   }
   // Try to login again
-  const exitCode = await execute('aws sso login')
+  const debugStream = new Transform({
+    transform(chunk, encoding, callback) {
+      // Pass the data through to process.stdout
+      this.push(chunk);
+      // `yarn install` progress bar deletes the last line which contains the login code
+      // so we add a new line to the end of the output
+      this.push("\n\n")
+      callback();
+    }
+  });
+  debugStream.pipe(process.stdout);
+  const exitCode = await execute('aws sso login', [], { stdout: debugStream })
   if (exitCode) {
     throw error
   }
